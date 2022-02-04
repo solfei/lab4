f52d55ec238ed4eb9fc98907c05d16f011622742
solfei <svaldima008@gmail.com>
2022-02-03 22:27:05 -0800

    hello world

---
+++ src/BST.java
@@ -0,0 +1,467 @@
+
+/**
+ * BST.java
+ * @author
+ * @author
+ * CIS 22C Lab 4
+ */
+
+import java.util.NoSuchElementException;
+
+public class BST<T extends Comparable<T>> {
+    private class Node {
+        private T data;
+        private Node left;
+        private Node right;
+
+        public Node(T data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    private Node root;
+
+    /*** CONSTRUCTORS ***/
+
+    /**
+     * Default constructor for BST
+     * sets root to null
+     */
+    public BST() {
+        root = null;
+    }
+
+    /**
+     * Copy constructor for BST
+     * 
+     * @param bst the BST to make
+     *            a copy of
+     */
+    public BST(BST<T> bst) {
+
+    }
+
+    /**
+     * Helper method for copy constructor
+     * 
+     * @param node the node containing
+     *             data to copy
+     */
+    private void copyHelper(Node node) {
+
+    }
+
+    /**
+     * Creates a BST of minimal height given an array of values
+     * 
+     * @param array the list of values to insert
+     * @precondition array must be sorted in ascending order
+     * @throws IllegalArgumentException when the array is
+     *                                  unsorted
+     */
+    public BST(T[] array) throws IllegalArgumentException {
+        boolean isSorted = true;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i].compareTo(array[i + 1]) > 0) {
+                isSorted = false;
+                break;
+            }
+        }
+        if (!isSorted) {
+            throw new IllegalArgumentException();
+        }
+
+        root = new Node(array[array.length / 2]);
+
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length / 2) {
+                continue;
+            }
+            this.insert(array[i]);
+        }
+
+    }
+
+    /*** ACCESSORS ***/
+
+    /**
+     * Returns the data stored in the root
+     * 
+     * @precondition !isEmpty()
+     * @return the data stored in the root
+     * @throws NoSuchElementException when
+     *                                precondition is violated
+     */
+    public T getRoot() throws NoSuchElementException {
+        if (root == null) {
+            throw new NoSuchElementException("no list, no root. sorry.");
+        }
+        return root.data;
+    }
+
+    /**
+     * Determines whether the tree is empty
+     * 
+     * @return whether the tree is empty
+     */
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    /**
+     * Returns the current size of the
+     * tree (number of nodes)
+     * 
+     * @return the size of the tree
+     */
+    public int getSize() {
+        return -1;
+    }
+
+    /**
+     * Helper method for the getSize method
+     * 
+     * @param node the current node to count
+     * @return the size of the tree
+     */
+    private int getSize(Node node) {
+        return -1;
+    }
+
+    /**
+     * Returns the height of tree by
+     * counting edges.
+     * 
+     * @return the height of the tree
+     */
+    public int getHeight() {
+        return -2;
+    }
+
+    /**
+     * Helper method for getHeight method
+     * 
+     * @param node the current
+     *             node whose height to count
+     * @return the height of the tree
+     */
+    private int getHeight(Node node) {
+        return -2;
+    }
+
+    /**
+     * Returns the smallest value in the tree
+     * 
+     * @precondition !isEmpty()
+     * @return the smallest value in the tree
+     * @throws NoSuchElementException when the
+     *                                precondition is violated
+     */
+    public T findMin() throws NoSuchElementException {
+        return null;
+    }
+
+    /**
+     * Helper method to findMin method
+     * 
+     * @param node the current node to check
+     *             if it is the smallest
+     * @return the smallest value in the tree
+     */
+    private T findMin(Node node) {
+        return null;
+    }
+
+    /**
+     * Returns the largest value in the tree
+     * 
+     * @precondition !isEmpty()
+     * @return the largest value in the tree
+     * @throws NoSuchElementException when the
+     *                                precondition is violated
+     */
+    public T findMax() throws NoSuchElementException {
+        return null;
+    }
+
+    /**
+     * Helper method to findMax method
+     * 
+     * @param node the current node to check
+     *             if it is the largest
+     * @return the largest value in the tree
+     */
+    private T findMax(Node node) {
+        return null;
+    }
+
+    /*** MUTATORS ***/
+
+    /**
+     * Inserts a new node in the tree
+     * 
+     * @param data the data to insert
+     */
+    public void insert(T data) {
+        if (root == null) {
+            root = new Node(data);
+        } else {
+            insert(data, root);
+        }
+    }
+
+    /**
+     * Helper method to insert
+     * Inserts a new value in the tree
+     * 
+     * @param data the data to insert
+     * @param node the current node in the
+     *             search for the correct location
+     *             in which to insert
+     */
+    private void insert(T data, Node node) {
+        if (data.compareTo(node.data) < 0) {
+            if (node.left == null) {
+                node.left = new Node(data);
+            } else {
+                insert(data, node.left);
+            }
+
+        } else if (data.compareTo(node.data) > 0) {
+            if (node.right == null) {
+                node.right = new Node(data);
+            } else {
+                insert(data, node.right);
+            }
+        } else {
+            if (node.left == null) {
+                node.left = new Node(data);
+            } else {
+                Node temp = node.left;
+                node.left = new Node(data);
+                node.left.left = temp;
+            }
+        }
+    }
+
+    /**
+     * Removes a value from the BST
+     * 
+     * @param data the value to remove
+     * @precondition !isEmpty()
+     * @throws IllegalStateException when BST is empty
+     */
+    public void remove(T data) throws IllegalStateException {
+        if (this.isEmpty()) {
+            throw new IllegalStateException("remove(): Tree is empty");
+        }
+
+        if (root.data.compareTo(data) == 0 && root.left == null && root.right == null) {
+            root = null;
+        } else {
+            Node toRemove = remove(data, root);
+            // code to remove
+        }
+    }
+
+    /**
+     * Helper method to the remove method
+     * 
+     * @param data the data to remove
+     * @param node the current node
+     * @return an updated reference variable
+     */
+    private Node remove(T data, Node node) {
+        // if (node.data.compareTo(data) == 0 && node.left == null && node.right ==
+        // null) { // leaf
+        // node = null;
+        // }
+
+        // If node is null
+        // 1. return node
+        if (node == null) {
+            return node;
+        }
+        // 2. Otherwise, if value < the node's data
+        // 1.set node's leftchild equal to the recursive call of remove helper on node's
+        // leftchild
+        if (node.data.compareTo(data) < 0) {
+            node.left = remove(data, node.left);
+            // 3. Otherwise, if value > the node's data
+            // 1. set node's rightchild equal to the recursive call of remove helper on
+            // node's rightchild
+        } else if (node.data.compareTo(data) > 0) {
+            node.right = remove(data, node.right);
+        }
+
+        else {
+            // 4. Otherwise,
+            // 1. If node is a leaf node
+            // 1. Set node to null
+            if (node.left == null && node.right == null) {
+                node = null;
+            }
+            // 2. Otherwise if node has a leftchild but no rightchild
+            // 1. set the leftchild to be the node
+            else if (node.right == null && node.left != null) {
+                node = node.left;
+                // 3. Otherwise if node has a rightchild but no leftchild
+                // 1. set the rightchild to be the node
+            } else if (node.left == null && node.right != null) {
+                node = node.right;
+            } else {
+                // first move right
+                // then we check a buncha left, for a left with no left of its own. smallest guy
+                // if the smallest guy is a leaf node, then node to remove = small guy.data, and
+                // smallguy = null
+                // if smallguy not leaf node, then nodetoremove.right = smallguy.right; and
+                // smallguy = null
+                Node temp = node.right;
+                if (temp.left == null) {
+                    node.data = temp.data;
+                    node.right = temp.right;
+                } else {
+                    while (temp.left.left != null) {
+                        temp = temp.left;
+                    }
+                    node.data = temp.left.data;
+                    temp.left = null;
+                }
+            }
+        }
+        return node; // step 5!
+        // 4. Otherwise
+        // 1. Search for the minimum value in the right subtree of the node
+        // 2. Set the node's data to be the minimum value in the node's right subtree
+        // 3. Set node's rightchild equal to the recursive call of remove helper,
+        // passing it node's お茶 (what?) and the minimum data of node's right subtree
+        // (i.e. delete the duplicate value in the right subtree)
+        // 5. return the node
+    }
+
+    /*** ADDITIONAL OPERATIONS ***/
+
+    /**
+     * Searches for a specified value
+     * in the tree
+     * 
+     * @param data the value to search for
+     * @return whether the value is stored
+     *         in the tree
+     */
+    public boolean search(T data) {
+        return false;
+    }
+
+    /**
+     * Helper method for the search method
+     * 
+     * @param data the data to search for
+     * @param node the current node to check
+     * @return whether the data is stored
+     *         in the tree
+     */
+    private boolean search(T data, Node node) {
+        return false;
+    }
+
+    /**
+     * Determines whether a BST is balanced
+     * using the definition given in the course
+     * lesson notes
+     * Note that we will consider an empty tree
+     * to be trivially balanced
+     * 
+     * @return whether the BST is balanced
+     */
+    public boolean isBalanced() {
+        if (root == null) {
+            return true;
+        }
+        return isBalanced(root);
+    }
+
+    /**
+     * Helper method for isBalanced
+     * to determine if a BST is balanced
+     * 
+     * @param n a Node in the tree
+     * @return whether the BST is balanced
+     *         at the level of the given Node
+     */
+    private boolean isBalanced(Node n) {
+        if (n != null) {
+            if (Math.abs(getHeight(n.left) - getHeight(n.right)) > 1) {
+                return false;
+            }
+            return isBalanced(n.left) && isBalanced(n.right);
+        }
+        return true;
+    }
+
+    /**
+     * Returns a String containing the data
+     * in post order
+     * 
+     * @return a String of data in post order
+     */
+    public String preOrderString() {
+        return "\n";
+    }
+
+    /**
+     * Helper method to preOrderString
+     * Inserts the data in pre order into a String
+     * 
+     * @param node     the current Node
+     * @param preOrder a String containing the data
+     */
+    private void preOrderString(Node node, StringBuilder preOrder) {
+
+    }
+
+    /**
+     * Returns a String containing the data
+     * in order
+     * 
+     * @return a String of data in order
+     */
+    public String inOrderString() {
+        return "\n";
+    }
+
+    /**
+     * Helper method to inOrderString
+     * Inserts the data in order into a String
+     * 
+     * @param node    the current Node
+     * @param inOrder a String containing the data
+     */
+    private void inOrderString(Node node, StringBuilder inOrder) {
+
+    }
+
+    /**
+     * Returns a String containing the data
+     * in post order
+     * 
+     * @return a String of data in post order
+     */
+    public String postOrderString() {
+        return "\n";
+    }
+
+    /**
+     * Helper method to postOrderString
+     * Inserts the data in post order into a String
+     * 
+     * @param node      the current Node
+     * @param postOrder a String containing the data
+     */
+    private void postOrderString(Node node, StringBuilder postOrder) {
+
+    }
+
+}
---
+++ src/BSTTest.java
@@ -0,0 +1,270 @@
+
+/**
+ * JUnit Test File for BST.java
+ * @author Jennifer Parrish
+ * CIS 22C, Lab 4
+ */
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.Test;
+
+class BSTTest {
+
+    @Test
+    void testBST() {
+        BST<String> bst = new BST<>();
+        assertTrue(bst.isEmpty());
+        assertEquals(0, bst.getSize());
+        assertThrows(NoSuchElementException.class, () -> {
+            bst.getRoot();
+        });
+    }
+
+    @Test
+    void testBSTBSTOfT() {
+        BST<String> nullBst = null;
+        BST<String> b = new BST<>(nullBst);
+        assertTrue(b.isEmpty());
+
+        BST<String> bst4 = new BST<>();
+        bst4.insert("5");
+        bst4.insert("1");
+        bst4.insert("2");
+        bst4.insert("4");
+        bst4.insert("6");
+        bst4.insert("8");
+        BST<String> bst = new BST<>(bst4);
+        assertEquals("5", bst.getRoot());
+        assertEquals(3, bst.getHeight());
+        bst.insert("3");
+        assertEquals(4, bst.getHeight());
+        assertEquals(3, bst4.getHeight());
+
+    }
+
+    @Test
+    void testBSTArray() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            BST<String> bst1 = new BST<>(new String[] { "C", "B", "A" });
+        });
+        String[] array = null;
+        BST<String> bst2 = new BST<>(array);
+        assertTrue(bst2.isEmpty());
+        BST<String> bst3 = new BST<>(new String[] { "A", "B", "C", "D", "E" });
+        assertEquals("C", bst3.getRoot());
+        assertEquals(2, bst3.getHeight());
+        BST<String> bst4 = new BST<>(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" });
+        assertEquals("5", bst4.getRoot());
+        assertEquals(3, bst4.getHeight());
+
+    }
+
+    @Test
+    void testGetRoot() {
+        BST<String> bst = new BST<>();
+        assertThrows(NoSuchElementException.class, () -> {
+            bst.getRoot();
+        });
+        bst.insert("F");
+        bst.insert("A");
+        bst.insert("C");
+        bst.insert("P");
+        bst.insert("Q");
+        bst.insert("Z");
+        bst.insert("L");
+        assertEquals("F", bst.getRoot());
+        bst.remove("F");
+        assertEquals("L", bst.getRoot());
+    }
+
+    @Test
+    void testIsEmpty() {
+        BST<String> bst = new BST<>();
+        assertTrue(bst.isEmpty());
+        bst.insert("C");
+        assertFalse(bst.isEmpty());
+        bst.insert("A");
+        bst.insert("B");
+        bst.insert("D");
+        assertFalse(bst.isEmpty());
+        bst.remove("C");
+        bst.remove("D");
+        bst.remove("A");
+        bst.remove("B");
+        assertTrue(bst.isEmpty());
+    }
+
+    @Test
+    void testGetSize() {
+        BST<String> bst = new BST<>();
+        assertEquals(0, bst.getSize());
+        bst.insert("C");
+        assertEquals(1, bst.getSize());
+        bst.insert("A");
+        bst.insert("B");
+        bst.insert("D");
+        assertEquals(4, bst.getSize());
+        bst.remove("C");
+        bst.remove("D");
+        bst.remove("A");
+        bst.remove("B");
+        assertEquals(0, bst.getSize());
+    }
+
+    @Test
+    void testGetHeight() {
+        BST<String> bst = new BST<>();
+        assertEquals(-1, bst.getHeight());
+        bst.insert("C");
+        assertEquals(0, bst.getHeight());
+        bst.insert("A");
+        bst.insert("D");
+        assertEquals(1, bst.getHeight());
+        bst.insert("B");
+        assertEquals(2, bst.getHeight());
+    }
+
+    @Test
+    void testFindMin() {
+        BST<String> bst = new BST<>();
+        assertThrows(NoSuchElementException.class, () -> {
+            bst.findMin();
+        });
+        BST<String> bst4 = new BST<>(new String[] { "10", "2", "3", "4", "5", "6", "7", "8", "9" });
+        assertEquals("10", bst4.findMin());
+    }
+
+    @Test
+    void testFindMax() {
+        BST<String> bst = new BST<>();
+        assertThrows(NoSuchElementException.class, () -> {
+            bst.findMax();
+        });
+        BST<String> bst4 = new BST<>(new String[] { "10", "2", "3", "4", "5", "6", "7", "8", "9" });
+        assertEquals("9", bst4.findMax());
+    }
+
+    @Test
+    void testSearch() {
+        BST<String> bst1 = new BST<>();
+        assertFalse(bst1.search("A"));
+        BST<String> bst4 = new BST<>(new String[] { "10", "2", "3", "4", "5", "6", "7", "8", "9" });
+        assertTrue(bst4.search("10"));
+        assertFalse(bst4.search("1"));
+    }
+
+    @Test
+    void testInsert() {
+        BST<String> bst = new BST<>();
+        bst.insert("ME");
+        bst.insert("MAY");
+        bst.insert("MAH");
+        bst.insert("MO");
+        bst.insert("MOO");
+        assertEquals("ME", bst.getRoot());
+        assertEquals(5, bst.getSize());
+        assertEquals(2, bst.getHeight());
+
+    }
+
+    @Test
+    void testRemove() {
+        BST<String> bst = new BST<>(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" });
+        bst.remove("5");
+        assertEquals("6", bst.getRoot());
+        BST<String> states = new BST<>();
+        states.insert("HI");
+        states.insert("MN");
+        states.insert("CA");
+        states.insert("IA");
+        states.insert("MI");
+        states.insert("AK");
+        states.remove("HI");
+        assertEquals("IA", states.getRoot());
+        states.remove("MN");
+        assertEquals("IA", states.getRoot());
+        assertEquals(4, states.getSize());
+        states.remove("CT");
+        assertEquals(4, states.getSize());
+        states.remove("IA");
+        assertEquals("MI", states.getRoot());
+        states.remove("AK");
+        assertEquals(2, states.getSize());
+        states.remove("CA");
+        assertFalse(states.isEmpty());
+        states.remove("MI");
+        assertTrue(states.isEmpty());
+        assertThrows(IllegalStateException.class, () -> {
+            states.remove("MI");
+        });
+    }
+
+    @Test
+    void testIsBalanced() {
+        BST<String> a = new BST<>();
+        assertTrue(a.isBalanced());
+        a = new BST<>(new String[] { "A", "B", "C", "D", "E", "F", "G" });
+        assertTrue(a.isBalanced());
+        a = new BST<>();
+        a.insert("C");
+        a.insert("B");
+        a.insert("A");
+        assertFalse(a.isBalanced());
+        a.insert("D");
+        a.insert("E");
+        assertTrue(a.isBalanced());
+        a.insert("F");
+        assertFalse(a.isBalanced());
+    }
+
+    @Test
+    void testPreOrderString() {
+        BST<String> empty = new BST<>();
+        assertEquals("\n", empty.preOrderString());
+
+        BST<String> states = new BST<>();
+        states.insert("HI");
+        states.insert("MN");
+        states.insert("CA");
+        states.insert("IA");
+        states.insert("MI");
+        states.insert("AK");
+
+        assertEquals("HI CA AK MN IA MI \n", states.preOrderString());
+    }
+
+    @Test
+    void testInOrderString() {
+        BST<String> empty = new BST<>();
+        assertEquals("\n", empty.inOrderString());
+
+        BST<String> states = new BST<>();
+        states.insert("HI");
+        states.insert("MN");
+        states.insert("CA");
+        states.insert("IA");
+        states.insert("MI");
+        states.insert("AK");
+
+        assertEquals("AK CA HI IA MI MN \n", states.inOrderString());
+    }
+
+    @Test
+    void testPostOrderString() {
+        BST<String> empty = new BST<>();
+        assertEquals("\n", empty.postOrderString());
+
+        BST<String> states = new BST<>();
+        states.insert("HI");
+        states.insert("MN");
+        states.insert("CA");
+        states.insert("IA");
+        states.insert("MI");
+        states.insert("AK");
+
+        assertEquals("AK CA MI IA MN HI \n", states.postOrderString());
+    }
+
+}
